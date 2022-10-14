82972b16bcd96f03b4573632c4671ed2379dac28
SomnusKKK <112879584+SomnusKKK@users.noreply.github.com>
2022-10-14 18:24:49 +0800

    Revert "Merge branch 'main' of https://github.com/Andy15/No-Hurry"
    This reverts commit 148f45aae1aa927e5c2b7d696d8499af403d1ce7, reversing
    changes made to 0d73b62199e3d2f0cfdb74b69e5a88e211b70183.

--- manage.py
+++ manage.py
@@ -1,42 +1,31 @@
-import os
-import base64
-import numpy as np
 import pandas as pd
-import face_recognition
 
-def add(csv, name, sex, sno, college, signed, photo, cache, tmp):
-    b = base64.b64decode(photo)
-    f = open(tmp, 'wb')
-    f.write(b)
-    f.close()
-    img = face_recognition.load_image_file(tmp)
-    arr = face_recognition.face_encodings(img)[0]
-    np.save(os.path.join(cache, 'sno' + '.npy'), arr)
-    pd.concat([pd.read_csv(csv, names=['name', 'sex', 'sno', 'college', 'signed']),
-               pd.DataFrame([[name, sex, sno, college, signed]],
-                            columns=['name', 'sex', 'sno', 'college', 'signed'])],
+def add(csv, name, sex, sno, photo, college, signed):
+    pd.concat([pd.read_csv(csv, names=['name', 'sex', 'sno', 'photo', 'college', 'signed']),
+               pd.DataFrame([[name, sex, sno, photo, college, signed]],
+                            columns=['name', 'sex', 'sno', 'photo', 'college', 'signed'])],
                ignore_index=True).to_csv(csv, header=None, index=False)
 
 def delete(csv, sno):
-    df = pd.read_csv(csv, names=['name', 'sex', 'sno', 'college', 'signed'])
+    df = pd.read_csv(csv, names=['name', 'sex', 'sno', 'photo', 'college', 'signed'])
     df.drop(index=df[df['sno'] == sno].index.tolist()).to_csv(csv, header=None, index=False)
 
 def manual(csv, sno):
-    df = pd.read_csv(csv, names=['name', 'sex', 'sno', 'college', 'signed'])
+    df = pd.read_csv(csv, names=['name', 'sex', 'sno', 'photo', 'college', 'signed'])
     df.loc[df['sno'] == sno, 'signed'] = '1'
     df.to_csv(csv, header=None, index=False)
 
 def all(csv):
     out = []
-    df = pd.read_csv(csv, names=['name', 'sex', 'sno', 'college', 'signed'])
+    df = pd.read_csv(csv, names=['name', 'sex', 'sno', 'photo', 'college', 'signed'])
     for idx, row in df.iterrows():
-        out.append({'name': row['name'], 'sex': row['sex'], 'sno': row['sno'], 'college': row['college'], 'signed': row['signed']})
+        out.append({'name': row['name'], 'sex': row['sex'], 'sno': row['sno'], 'photo': row['photo'], 'college': row['college'], 'signed': row['signed']})
     return out
 
 def unsigned(csv):
     out = []
-    df = pd.read_csv(csv, names=['name', 'sex', 'sno', 'college', 'signed'])
+    df = pd.read_csv(csv, names=['name', 'sex', 'sno', 'photo', 'college', 'signed'])
     for idx, row in df.iterrows():
         if row['signed'] == 1:
-            out.append({'name': row['name'], 'sex': row['sex'], 'sno': row['sno'], 'college': row['college'], 'signed': row['signed']})
+            out.append({'name': row['name'], 'sex': row['sex'], 'sno': row['sno'], 'photo': row['photo'], 'college': row['college'], 'signed': row['signed']})
     return out
--- model.py
+++ model.py
@@ -1,5 +1,4 @@
 import os
-import base64
 import numpy as np
 import pandas as pd
 import face_recognition
@@ -9,22 +8,23 @@ face = []
 res = set()
 
 def init(csv, cache):
-    df = pd.read_csv(csv, names=['name', 'sex', 'sno', 'college', 'signed'])
+    df = pd.read_csv(csv, names=['name', 'sex', 'sno', 'photo', 'college', 'signed'])
 
     for idx, row in df.iterrows():
         if row['sno'] in name:
             continue
-        arr = np.load(os.path.join(cache, row['sno'] + '.npy'))
+        if os.path.exists(os.path.join(cache, row['sno'] + '.npy')):
+            arr = np.load(os.path.join(cache, row['sno'] + '.npy'))
+        else:
+            img = face_recognition.load_image_file(row['photo'])
+            arr = face_recognition.face_encodings(img)[0]
+            np.save(os.path.join(cache, row['sno'] + '.npy'), arr)
         name.append(row['sno'])
         face.append(arr)
 
-def test(photo, threshold, tmp):
+def test(path, threshold):
     try:
-        b = base64.b64decode(photo)
-        f = open(tmp, 'wb')
-        f.write(b)
-        f.close()
-        img = face_recognition.load_image_file(tmp)
+        img = face_recognition.load_image_file(path)
         loc = face_recognition.face_locations(img)
 
         for i in range(len(loc)):
